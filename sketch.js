e9a621add48b660c8640823765e9b317e505bed0
quangnle <mnest2409@yahoo.com>
2024-08-22 14:22:14 +0700

    update UI

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let w = 640, h = 480;
 let mDown = false;
 let points = [];
 let rPoints = [];
+let tolerance = 3;
 
 function setup(){
 	var canvas = createCanvas(w, h);
@@ -10,15 +11,19 @@ function setup(){
 
 function draw(){
 	background(255);
+	fill(255);
 	stroke(1);	
 	rect(0,0,w-1,h-1);
 	line(w >> 1, 1, w >> 1, h-1);
-	
+
+	fill(0);
+	text("Draw on this side, release to see the result on the right.", 10, 20);	
+
 	if (mDown){
-		if (mouseX < (w>>1)){
+		if (mouseX > 0 && mouseX < (w >> 1) && mouseY > 0 && mouseY < h){
 			points.push(new Vec2(mouseX, mouseY));
 		}
-	}
+	} 
 	
 	for (let i = 0; i < points.length - 1; i++){
 		line(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
@@ -27,7 +32,7 @@ function draw(){
 	if (rPoints.length > 0){
 		push();
 		translate(w >> 1, 0);
-		
+
 		// for (let i = 0; i < rPoints.length - 1; i++){
 		// 	line(rPoints[i].x, rPoints[i].y, rPoints[i+1].x, rPoints[i+1].y);
 		// }
@@ -49,10 +54,15 @@ function mousePressed(){
 function mouseReleased(){
 	mDown = false;
 	if (points.length > 0){
-		rPoints = dpReduction(points, 3);
+		rPoints = dpReduction(points, tolerance);
 		if (rPoints != null && rPoints.length > 1){
 			fit(rPoints, 4.0, result);
+
+			const lblNPoints = document.getElementById('nPoints');
+			lblNPoints.innerHTML = `Number of points: <b>${points.length}</b>`;
+
+			const lblRPoints = document.getElementById('nRPoints');
+			lblRPoints.innerHTML = `Number of points after reduction: <b>${rPoints.length}</b>`;
 		}
-	}
-	
+	}	
 }
